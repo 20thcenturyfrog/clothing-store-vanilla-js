9fdf26b14b2d672eaf7302cf1d4dd107d5b56298
Polina <polinscik@gmail.com>
2023-04-08 21:07:00 +0300

    add styles (not complete)

---
+++ assets/js/product-page.js
@@ -0,0 +1,21 @@
+import {
+  footerLogoImg,
+  footerPrivacyPolicy,
+  footerPublicOffer,
+  footerTel,
+  footerEmail,
+  footerInstagramIcon,
+  footerFacebookIcon,
+  footerTwitterIcon,
+  footerVisaImg,
+} from "./elements/footer.js";
+
+footerLogoImg.attachLink("#");
+footerPrivacyPolicy.attachLink("#");
+footerPublicOffer.attachLink("#");
+footerTel.attachLink("#");
+footerEmail.attachLink("#");
+footerInstagramIcon.attachLink("#");
+footerFacebookIcon.attachLink("#");
+footerTwitterIcon.attachLink("#");
+footerVisaImg.attachLink("#");
